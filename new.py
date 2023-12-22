a32b7196823902125043e0fc63a7f200b2e10bd1
rutuja36 <gawaderutuja38@gmail.com>
2023-12-23 03:30:07 +0530

    neww

--- new.py
+++ new.py
@@ -6,9 +6,43 @@ tk = Tk()
 tk.title("Login System")
 tk.geometry("400x250")  # Corrected the typo in the geometry size
 tk.resizable(False, False)
+<<<<<<< HEAD
 tk.configure(bg="white")                #blue
 
 def login():
+=======
+tk.configure(bg="cadetblue")                #blue
+
+# for database
+def login_data():
+    import mysql.connector
+
+    mydb = mysql.connector.connect(host='localhost',port=3306,user='root',password='Admin@123',database='login_db')
+
+    mycursor = mydb.cursor()
+
+    username = entry1.get()
+    password = entry2.get()
+
+
+    mycursor.execute("insert into login values (%s,%s)",(username,password))
+    mydb.commit()
+
+    if username == "" and password == "":
+        messagebox.showerror("Error", "Blank not allowed")
+    elif username == "":
+        messagebox.showinfo("Error", "Please enter username")
+    elif password == "":
+        messagebox.showinfo("Error", "Please enter password")
+    elif username == "ditiss" and password == "iacsd@123":
+        messagebox.showinfo("Success", "Login successful")
+    else:
+        messagebox.showerror("Oops", "Invalid username/password")
+
+
+def login():
+    '''
+>>>>>>> 4fdf8c5 (new1)
     username = entry1.get()
     password = entry2.get()
 
@@ -22,7 +56,11 @@ def login():
         messagebox.showinfo("Success", "Login successful")
     else:
         messagebox.showerror("Oops", "Invalid username/password")
+<<<<<<< HEAD
 
+=======
+    '''
+>>>>>>> 4fdf8c5 (new1)
 # Label
 Label(tk, text="welcome to iacsd", font="impact 20 bold", bg="black", fg="red").pack(fill=X)
 
@@ -36,7 +74,11 @@ entry2 = Entry(tk, bd=4, show="*")
 entry2.place(x=130, y=120)
 
 # Button
+<<<<<<< HEAD
 Button(tk, text="Login", bg="green", command=login).place(x=150, y=160)
+=======
+Button(tk, text="Login", bg="green", command=login_data).place(x=150, y=160)
+>>>>>>> 4fdf8c5 (new1)
 
 tk.mainloop()
 
