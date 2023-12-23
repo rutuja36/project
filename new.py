1ca5f007d259d5429cd2c9f9dbfad96326f94dc7
rutuja36 <gawaderutuja38@gmail.com>
2023-12-23 05:02:43 +0000

    new

--- new.py
+++ new.py
@@ -6,12 +6,8 @@ tk = Tk()
 tk.title("Login System")
 tk.geometry("400x250")  # Corrected the typo in the geometry size
 tk.resizable(False, False)
-<<<<<<< HEAD
 tk.configure(bg="white")                #blue
 
-def login():
-=======
-tk.configure(bg="cadetblue")                #blue
 
 # for database
 def login_data():
@@ -41,26 +37,10 @@ def login_data():
 
 
 def login():
-    '''
->>>>>>> 4fdf8c5 (new1)
-    username = entry1.get()
-    password = entry2.get()
 
-    if username == "" and password == "":
-        messagebox.showerror("Error", "Blank not allowed")
-    elif username == "":
-        messagebox.showinfo("Error", "Please enter username")
-    elif password == "":
-        messagebox.showinfo("Error", "Please enter password")
-    elif username == "ditiss" and password == "iacsd@123":
-        messagebox.showinfo("Success", "Login successful")
-    else:
-        messagebox.showerror("Oops", "Invalid username/password")
-<<<<<<< HEAD
 
-=======
-    '''
->>>>>>> 4fdf8c5 (new1)
+	tk.configure(bg="cadetblue")                #blue
+
 # Label
 Label(tk, text="welcome to iacsd", font="impact 20 bold", bg="black", fg="red").pack(fill=X)
 
@@ -74,11 +54,8 @@ entry2 = Entry(tk, bd=4, show="*")
 entry2.place(x=130, y=120)
 
 # Button
-<<<<<<< HEAD
 Button(tk, text="Login", bg="green", command=login).place(x=150, y=160)
-=======
 Button(tk, text="Login", bg="green", command=login_data).place(x=150, y=160)
->>>>>>> 4fdf8c5 (new1)
 
 tk.mainloop()
 
